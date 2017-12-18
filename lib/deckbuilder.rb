7d40e0f1bd6c270b663dfb5e13e2f9409e96b4b7
Josh Frey <freyjt@gmail.com>
2017-12-17 22:31:52 -0500

    Red build. Working on discard being able to take correct action if bad token is passed

--- lib/deckbuilder.rb
+++ lib/deckbuilder.rb
@@ -4,6 +4,8 @@
 class DeckBuilder
   def initialize(path_to_deck)
     @initial_deck = load_from_file(path_to_deck)
+    @deck = [@initial_deck]
+    @discard = []
   end
 
   def load_from_file(path_to_deck)
@@ -28,4 +30,28 @@ class DeckBuilder
 
   end
 
+  def move_to_discard(move_token)
+    last_deck = @deck.last
+    removed_card = delete_first(last_deck, move_token)
+    @deck.pop if(last_deck.length == 0)
+    @discard.push removed_card
+  end
+
+  def discard_spy
+    @discard.clone
+  end
+
+  # What's left of starting deck is always @ 0
+  def bottom_deck_spy
+    @deck.first.clone
+  end
+
+  private
+
+  def delete_first(deck, item_token)
+    deck.each_with_index do |deck_item, i|
+      return deck.delete_at(i) if(deck_item["displayToken"] == item_token)
+    end
+    raise Error # @TODO Name errors, this one happens because we tried to delete a card that wasn't in the top deck
+  end
 end
--- spec/deckbuilder_spec.rb
+++ spec/deckbuilder_spec.rb
@@ -6,6 +6,7 @@ require_relative '../env.rb'
 describe DeckBuilder do
 
   PATH_TO_DEFAULT = "./resources/default_cities.json"
+  DEFAULT_DECK_SIZE = 27
   NY_OBJECT = { "displayOption" => "New York", "displayToken" => "NY" }
 
   before(:each) do
@@ -14,13 +15,23 @@ describe DeckBuilder do
 
   it "#read_from_file returns array of 1 element for each card in deck" do
     default_deck = @deck_builder.load_from_file(PATH_TO_DEFAULT)
-    expect(default_deck.length).to eq(27) # starting size of pandemic legacy season 2
+    expect(default_deck.length).to eq(DEFAULT_DECK_SIZE) # starting size of pandemic legacy season 2
   end
 
   it "#read_from_file default includes object NY" do
     default_deck = @deck_builder.load_from_file(PATH_TO_DEFAULT)
-    expect(default_deck).to include(NY_OBJECT) # starting size of pandemic legacy season 2
+    expect(default_deck).to include(NY_OBJECT)
     
+  end
+
+  it "#move_to_discard increases discard by 1" do
+     @deck_builder.move_to_discard("NY")
+     expect(@deck_builder.bottom_deck_spy.length).to be(DEFAULT_DECK_SIZE - 1)
+     expect(@deck_builder.discard_spy.length).to be( 1 )
+  end
+
+  it "#move_to_discard breaks when the token is not known" do
+     @deck_builder.move_to_discard("Ottawa")
   end
 
 end
